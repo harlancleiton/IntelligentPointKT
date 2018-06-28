f90654609da053ccbc8e58ea52e273e0bbefa52d
Harlan Cleiton <harlancleiton@gmail.com>
2018-06-27 22:18:43 -0300

    First commit.

---
+++ src/main/kotlin/br/harlan/intelligentpoint/IntelligentPointApplication.kt
@@ -0,0 +1,11 @@
+package br.harlan.intelligentpoint
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntelligentPointApplication
+
+fun main(args: Array<String>) {
+    runApplication<IntelligentPointApplication>(*args)
+}
